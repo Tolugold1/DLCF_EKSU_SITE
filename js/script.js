f3aba85ce6424dfa08b62072e5a13e5d9482151c
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-03-26 10:23:06 +0100

    update

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* var btn = document.querySelctor('button');
-var create = document.createElement("button")
-var aria = document.createTextNode("class=\"navbar-toggler collapsed\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\"\>);
-var add = btn.
-btn.addEventListener('click', function(){
-    aria.innerHTML = "true";
-}); */
